23740fd553fb5bc3e414f1cda6b40d5fdc8f0229
akpil <jyk0802@gmail.com>
2018-12-26 17:47:21 +0900

    Roll a ball Set Player, ground, wall

---
+++ RollABall/Assets/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    private Rigidbody rb;
+    public int Speed;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float hori = Input.GetAxis("Horizontal");
+        float vert = Input.GetAxis("Vertical");
+        //Debug.LogFormat("{0}\n{1}", hori.ToString(), vert.ToString());
+        if (Input.GetButtonDown("Fire1"))
+        {
+            gameObject.SetActive(false);
+        }
+        //rb.AddForce(new Vector3(hori, 0, vert) * Speed);
+	}
+}

f7ab3ef9f5f7d8bf04e0a2fc5054db52cf35ae32
akpil <jyk0802@gmail.com>
2019-01-11 17:53:39 +0900

    2Dgame Animation

---
+++ ActionGame/Assets/Scripts/AnimationHashList.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationHashList : MonoBehaviour {
+    public static readonly int isAttackHash = Animator.StringToHash("IsAttack");
+    public static readonly int isDeadHash = Animator.StringToHash("IsDead");
+
+}
---
+++ ActionGame/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    private Animator anim;
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            anim.SetBool(AnimationHashList.isAttackHash, true);
+        }
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            anim.SetBool(AnimationHashList.isAttackHash, false);
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            anim.SetBool(AnimationHashList.isDeadHash, true);
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            anim.SetBool(AnimationHashList.isDeadHash, false);
+        }
+    }
+}

3e29e552a5670cb2cc68a3de4ace620f5f04da43
akpil <jyk0802@gmail.com>
2019-01-14 17:51:20 +0900

    enemy Animation, physics layer, animation event

--- ActionGame/Assets/Scripts/AnimationHashList.cs
+++ ActionGame/Assets/Scripts/AnimationHashList.cs
@@ -5,5 +5,6 @@ using UnityEngine;
 public class AnimationHashList : MonoBehaviour {
     public static readonly int isAttackHash = Animator.StringToHash("IsAttack");
     public static readonly int isDeadHash = Animator.StringToHash("IsDead");
+    public static readonly int isWalkHash = Animator.StringToHash("IsWalk");
 
 }
---
+++ ActionGame/Assets/Scripts/EnemeyController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemeyController : MonoBehaviour {
+    private Animator anim;
+    private Rigidbody2D rb2D;
+    private float speed;
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+        rb2D = GetComponent<Rigidbody2D>();
+        speed = 2f;
+    }
+
+    private void OnEnable()
+    {
+        rb2D.velocity = transform.right * speed;
+        anim.SetBool(AnimationHashList.isWalkHash, true);
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void FinishAttack()
+    {
+        anim.SetBool(AnimationHashList.isAttackHash, false);
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Find player");
+            anim.SetBool(AnimationHashList.isWalkHash, false);
+            anim.SetBool(AnimationHashList.isAttackHash, true);
+        }
+    }
+}

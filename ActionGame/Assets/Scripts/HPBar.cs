16cd9709ef4276626ae7f6db3d4b53e3c8b61fd5
akpil <jyk0802@gmail.com>
2019-01-17 17:47:57 +0900

    HPBar finish

--- ActionGame/Assets/Scripts/HPBar.cs
+++ ActionGame/Assets/Scripts/HPBar.cs
@@ -4,12 +4,31 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class HPBar : MonoBehaviour {
-
+    [SerializeField]
+    private GameObject hpBarObj;
     [SerializeField]
     private Image hpBar;
 
+    [SerializeField]
+    private GameObject incomeObj;
+    [SerializeField]
+    private Text income;
+
+    private void OnEnable()
+    {
+        hpBarObj.SetActive(false);
+        incomeObj.SetActive(false);
+    }
+
     public void ShowHP(float amount)
     {
+        hpBarObj.SetActive(true);
         hpBar.fillAmount = amount;
+    }
+
+    public void ShowIncome(float amount)
+    {
+        incomeObj.SetActive(true);
+        income.text = amount.ToString("F1");
     }
 }
